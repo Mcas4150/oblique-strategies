37b635f9aff28fc121bf7fae12586656de357eb9
Michael Cassidy <mikecassidy0000@gmail.com>
2018-03-09 18:20:26 -0500

    color

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -2,29 +2,37 @@ import React, { Component } from 'react';
 import CardsAPI from './cardAPI';
 import './card.css';
 
+
 class Card extends Component {
     constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
     this.state = {
-      random: "click to draw"
+      random: "Click",
+      backgroundColor : "lightgrey",
     };
   }
 
    handleClick() {
-      document.getElementById('advice').animate([
-         { transform: 'translateY(0px)' },
-         { transform: 'translateY(-50px)' }
-        ], { duration: 500
-        });
-      var randomCard = CardsAPI[Math.floor(Math.random() * CardsAPI.length)];
+      // document.getElementById('advice').animate([
+      //    { transform: 'translateX(0px)' },
+      //    { transform: 'translateX(-100px)' }
+      //   ], { duration: 100
+      //   });
+      let rgb = []
+      for (var i = 0; i < 3; i++) {
+        let r = Math.floor(Math.random() * 256)
+        rgb.push(r)
+      }
+     this.setState({ backgroundColor: `rgb(${rgb})`})
+      let randomCard = CardsAPI[Math.floor(Math.random() * CardsAPI.length)];
       this.setState({ random: randomCard });
   }
 
   render() {
     return (
-      <div className="Card" onClick={this.handleClick.bind(this)}>
-        <div id="advice">{this.state.random}</div>
+      <div className="Card animated bounce" onClick={this.handleClick.bind(this)} style={ this.state }>
+        <div id="advice" class="animated bounceInRight">{this.state.random}</div>
       </div>
     );
   }
