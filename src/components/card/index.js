4739f33e0b420978238f42bf1cdd246009d9771b
Michael Cassidy <mikecassidy0000@gmail.com>
2018-03-09 21:16:43 -0500

    refactor

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,39 +1,193 @@
 import React, { Component } from 'react';
 import CardsAPI from './cardAPI';
+import Fonts from './fonts';
+import Animations from './animate';
 import './card.css';
+import {bouceOut, bounce, bounceIn, bounceInDown, bounceInLeft, bounceInRight, bounceInUp,
 
+bounceOutDown,
+
+bounceOutLeft,
+
+bounceOutRight,
+
+bounceOutUp,
+
+fadeIn,
+
+fadeInDown,
+
+fadeInDownBig,
+
+fadeInLeft,
+
+fadeInLeftBig,
+
+fadeInRight,
+
+fadeInRightBig,
+
+fadeInUp,
+
+fadeInUpBig,
+
+fadeOut,
+
+fadeOutDown,
+
+fadeOutDownBig,
+
+fadeOutLeft,
+
+fadeOutLeftBig,
+
+fadeOutRight,
+
+fadeOutRightBig,
+
+fadeOutUp,
+
+fadeOutUpBig,
+
+flash,
+
+flip,
+flipInX,
+
+flipInY,
+
+flipOutX,
+
+flipOutY,
+
+headShake,
+
+hinge,
+
+jello,
+
+lightSpeedIn,
+
+lightSpeedOut,
+
+pulse,
+
+rollIn,
+
+rollOut,
+
+rotateIn,
+
+rotateInDownLeft,
+
+rotateInDownRight,
+
+rotateInUpLeft,
+
+rotateInUpRight,
+
+rotateOut,
+
+rotateOutDownLeft,
+
+rotateOutDownRight,
+
+rotateOutUpLeft
+,
+rotateOutUpRight,
+
+rubberBand,
+
+shake,
+
+slideInDown,
+
+slideInLeft,
+
+slideInRight,
+
+slideInUp,
+
+slideOutDown,
+
+slideOutLeft,
+
+slideOutRight,
+
+slideOutUp,
+
+swing,
+
+tada,
+
+wobble,
+
+zoomIn,
+
+zoomInDown,
+
+zoomInLeft,
+
+zoomInRight,
+zoomInUp, zoomOut, zoomOutDown, zoomOutLeft, zoomOutRight, zoomOutUp} from 'react-animations';
+import Radium, {StyleRoot} from 'radium';
+
+
+
+let randomCard = CardsAPI[Math.floor(Math.random() * CardsAPI.length)];
+
+
+// const styles = {
+//     fadeInUp: {
+//           animation: 'x 2s',
+//           animationName: Radium.keyframes(fadeInUp, 'fadeInUp')
+//         }
+// }
+   let randAnimate= Animations[Math.floor(Math.random() * Animations.length)];
 
 class Card extends Component {
     constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
+    let animatethis = [zoomInUp, zoomOut, zoomOutDown, zoomOutLeft, zoomOutRight, zoomOutUp];
+    let pleasework = animatethis[Math.floor(Math.random() * animatethis.length)]
     this.state = {
-      random: "Click",
+      random: randomCard,
       backgroundColor : "lightgrey",
+      color: "white",
+      animation: "x 1s",
+      animationName: Radium.keyframes(pleasework)
     };
   }
 
+
    handleClick() {
-      // document.getElementById('advice').animate([
-      //    { transform: 'translateX(0px)' },
-      //    { transform: 'translateX(-100px)' }
-      //   ], { duration: 100
-      //   });
       let rgb = []
       for (var i = 0; i < 3; i++) {
         let r = Math.floor(Math.random() * 256)
         rgb.push(r)
       }
+
+    // let randAnimate= Animations[Math.floor(Math.random() * Animations.length)];
+    //  this.setState({ animationName: Radium.keyframes(animateNo, '${animateNo}')})
+    let animatethis = [zoomInUp, zoomOut, zoomOutDown, zoomOutLeft, zoomOutRight, zoomOutUp];
+    let pleasework = animatethis[Math.floor(Math.random() * animatethis.length)]
+    this.setState({ animationName: Radium.keyframes(pleasework)})
      this.setState({ backgroundColor: `rgb(${rgb})`})
       let randomCard = CardsAPI[Math.floor(Math.random() * CardsAPI.length)];
       this.setState({ random: randomCard });
+      //  let randomFont = Fonts[Math.floor(Math.random() * Fonts.length)];
+     // this.setState({ backgroundColor: `rgb(${rgb})`, fontFamily: randomFont})
+
   }
 
   render() {
     return (
-      <div className="Card animated bounce" onClick={this.handleClick.bind(this)} style={ this.state }>
-        <div id="advice" class="animated bounceInRight">{this.state.random}</div>
-      </div>
+      <StyleRoot>
+        <div className="Card animated bounce" onClick={this.handleClick.bind(this)} style={ this.state}>
+          <div id="advice" style={this.state.animation, this.state.animationName, this.state.color}>{this.state.random}</div>
+        </div>
+      </StyleRoot>
     );
   }
 }
--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,15 +1,43 @@
 import React, { Component } from 'react';
 import './main.css';
 import Card from "../card/index";
+import CardsAPI from '../card/cardAPI';
+import Radium, {StyleRoot} from 'radium';
 
+
+let randomCard = CardsAPI[Math.floor(Math.random() * CardsAPI.length)];
 
 
 class Main extends Component {
+    constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+
+    this.state = {
+      random: randomCard,
+      backgroundColor : "lightgrey",
+      color: "white",
+    };
+  }
+
+   handleClick() {
+      let rgb = []
+      for (var i = 0; i < 3; i++) {
+        let r = Math.floor(Math.random() * 256)
+        rgb.push(r)
+      }
+     this.setState({ backgroundColor: `rgb(${rgb})`})
+      let randomCard = CardsAPI[Math.floor(Math.random() * CardsAPI.length)];
+      this.setState({ random: randomCard });
+  }
+
   render() {
     return (
-      <div className="Main">
-        <Card/>
-      </div>
+      <StyleRoot>
+        <div className="Main animated bounce" onClick={this.handleClick.bind(this)} style={ this.state}>
+          <div id="advice">{this.state.random}</div>
+        </div>
+      </StyleRoot>
     );
   }
 }
